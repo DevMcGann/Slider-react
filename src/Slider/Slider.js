0b75fa37c4bdcabdc09f8a52584c721156d39081
DevMcGann <gab.mcganicus@gmail.com>
2020-02-04 23:56:09 -0300

    primer commit

---
+++ src/Slider/Slider.js
@@ -0,0 +1,37 @@
+import React,{useState} from 'react';
+import './slider.scss';
+import SliderItem from './SliderItem.js'
+import i1 from '../Pics/iphones.jpg'
+import i2 from '../Pics/huaweis.jpg'
+import i3 from '../Pics/xiaomis.jpg'
+
+
+const Slider = () => {
+
+    let arr = [<SliderItem src={i1}/>,<SliderItem src={i2}/>,<SliderItem src={i3}/>];
+    const [x,setX] = useState(0);
+
+    const goLeft = () => {
+       x === 0 ? setX(-100 * (arr.length -1)) : setX(x+100); 
+    }
+
+    const goRight = () => {
+        x === -100 *(arr.length -1) ? setX(0) : setX(x - 100);
+    }
+
+    return ( 
+        <div className="slider">
+            {arr.map((item,index)=>{
+                return(
+                    <div key={index} className="slide" style={{transform:`translateX(${x}%)`}}>
+                        {item}
+                    </div>
+                )
+            })}
+            <button id="goLeft" onClick={goLeft}><i className="fas fa-chevron-left"></i></button>
+            <button id="goRight" onClick={goRight}><i className="fas fa-chevron-right"></i></button>
+        </div>
+     );
+}
+ 
+export default Slider;
---
+++ src/Slider/SliderItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SliderItem = ({src}) => {
+
+
+  
+    return (
+        <img src={src} alt="foto"/>
+      );
+}
+ 
+export default SliderItem;
